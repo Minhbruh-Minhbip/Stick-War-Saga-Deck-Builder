e510f19e6a98c19918be92234ad2b0f993755615
Minhbip <Minhbruh111@gmail.com>
2026-05-12 15:37:42 +0700

    AI Update

--- js/app.js
+++ js/app.js
@@ -693,6 +693,8 @@ window.loadDecksFromSupabase = async (mode) => {
     }
 };
 
+(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...args)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="DLOy2IJReRXiT6_Lcape4";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();
+
 let idleSeconds = 0;
 
 const resetIdle = () => { idleSeconds = 0; };
